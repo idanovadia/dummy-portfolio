dc49b4fd03b0036b51abe37c1c1ca7f2ad237d12
idan <idanov@post.bgu.ac.il>
2022-02-20 20:58:44 +0200

    fix number of projects in row

--- src/data.js
+++ src/data.js
@@ -6,27 +6,17 @@ export const products = [
     },
     {
       id: 2,
-      img: "https://i.ibb.co/rp8yJMM/screencapture-youtube-2022-02-16-14-25-56.png",
-      link: "https://www.youtube.com/",
+      img: "https://i.ibb.co/L8jbhjG/screencapture-fxp-co-il-2022-02-20-20-42-04.png",
+      link: "https://www.fxp.co.il/",
     },
     {
       id: 3,
-      img: "https://i.ibb.co/rp8yJMM/screencapture-youtube-2022-02-16-14-25-56.png",
-      link: "https://www.youtube.com/",
+      img: "https://i.ibb.co/ch1qt0s/screencapture-udemy-2022-02-20-20-39-21.png",
+      link: "https://www.udemy.com/",
     },
     {
       id: 4,
-      img: "https://i.ibb.co/rp8yJMM/screencapture-youtube-2022-02-16-14-25-56.png",
-      link: "https://www.youtube.com/",
-    },
-    {
-      id: 5,
-      img: "https://i.ibb.co/rp8yJMM/screencapture-youtube-2022-02-16-14-25-56.png",
-      link: "https://www.youtube.com/",
-    },
-    {
-      id: 6,
-      img: "https://i.ibb.co/rp8yJMM/screencapture-youtube-2022-02-16-14-25-56.png",
-      link: "https://www.youtube.com/",
+      img: "https://i.ibb.co/HTjbW7W/screencapture-stackoverflow-2022-02-20-20-39-55.png",
+      link: "https://stackoverflow.com/",
     },
   ];
